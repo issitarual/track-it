bbdc0f1410c7f9540a95fd62fd2037d7ee42bb92
Laura Tissi <laura.tracierra@gmail.com>
2021-05-18 20:12:48 -0300

    usando os dados da API para setUSer

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,39 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import UserContext from '../contexts/UserContexts';
 import Home from './Home/Home';
 import Register from './Register/Register';
 import Habits from './Habits/Habits';
 import Today from './Today/Today';
 import Historic from './Historic/Historic';
-import '../css/reset.css'
-import '../css/style.css'
+import '../css/reset.css';
+import '../css/style.css';
 
 export default function App(){
+    const [user, setUser] = useState("");
+    console.log(user);
+
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/cadastro" exact>
-                    <Register />
-                </Route>
-                <Route path="/habitos" exact>
-                    <Habits />
-                </Route>
-                <Route path="/hoje" exact>
-                    <Today />
-                </Route>
-                <Route path="/historico" exact>
-                    <Historic />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <UserContext.Provider value={{user, setUser}}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/cadastro" exact>
+                        <Register />
+                    </Route>
+                    <Route path="/habitos" exact>
+                        <Habits />
+                    </Route>
+                    <Route path="/hoje" exact>
+                        <Today />
+                    </Route>
+                    <Route path="/historico" exact>
+                        <Historic />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </UserContext.Provider>
     )
 }
