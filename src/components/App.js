d7e3b9c51ea203c1a1b8bdac7c36830f31dad94d
Laura Tissi <laura.tracierra@gmail.com>
2021-05-18 07:38:33 -0300

    layout da tela de entrar e registrat

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import Register from './Register/Register';
+import Habits from './Habits/Habits';
+import Today from './Today/Today';
+import Historic from './Historic/Historic';
+import '../css/reset.css'
+
+export default function App(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/cadastro" exact>
+                    <Register />
+                </Route>
+                <Route path="/habitos" exact>
+                    <Habits />
+                </Route>
+                <Route path="/hoje" exact>
+                    <Today />
+                </Route>
+                <Route path="/historico" exact>
+                    <Historic />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
